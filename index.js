f75ddd1b09b7a02f38e7bc9f463b8ff59dcc2f5c
Anna Fernandez <bri.ann518@gmail.com>
2017-09-28 18:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return ('Happy holidays, ${name}!')
 }
 
-function happyHolidaysTo(name) { 
+function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
 
@@ -15,7 +15,6 @@ function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday,days) { 
+function holidayCountdown(holiday,days) {
   return`It's ${days} days until ${holiday}!`
 }
-
